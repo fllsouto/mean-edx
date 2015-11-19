9a39d744f6cc9886bf750d304ef3c12deac729ec
Fellipe Souto <fllsouto@gmail.com>
2015-11-18 22:36:39 -0200

    a quick change on underscore declaration to test if I need a specific name for the required variable

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // require is a nodejs build in method to import packages and apps
-var _ = require('underscore');
-_.each([1,2,3,4], function(v){
+var underfoo = require('underscore');
+underfoo.each([1,2,3,4], function(v){
   console.log(v);
 });
